4b02bbaaf83d67491b70dd1811e4f74727a93eb2
Andres Zapata <me@azapata.io>
2015-12-23 17:15:26 -0500

    Add bowe.json with TWBT, Backbone and jQuery, set up directory conventions

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,31 +1,19 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Routes File
-|--------------------------------------------------------------------------
-|
-| Here is where you will register all of the routes in an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
-
+// Home Route
 Route::get('/', function () {
-    return view('welcome');
+
+    return view('home');
 });
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| This route group applies the "web" middleware group to every route
-| it contains. The "web" middleware group is defined in your HTTP
-| kernel and includes session state, CSRF protection, and more.
-|
-*/
+// Application Routes
 
 Route::group(['middleware' => ['web']], function () {
     //
 });
+
+// Api Routes
+Route::group(['prefix' => 'api/v1', 'namespace' => 'Api\V1',], function() {
+
+});
+
---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Garden Revolution</title>
+        <link rel="stylesheet" href="{{ asset('css/home.css') }}">
+    </head>
+    <body>
+    </body>
+</html>
