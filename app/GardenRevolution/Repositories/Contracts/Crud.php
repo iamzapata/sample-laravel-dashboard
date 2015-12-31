f19d190e1658389f25f9708082bf2f185a7f48cc
Andres Zapata <me@azapata.io>
2015-12-30 21:52:07 -0500

    Refactor UserRepository

--- app/GardenRevolution/Repositories/Contracts/Crud.php
+++ app/GardenRevolution/Repositories/Contracts/Crud.php
@@ -5,7 +5,8 @@
  * @author Alan Ruvalcaba
  * @since 2015-12-28
  */
-interface Crud {
+interface Crud 
+{
     function create(array $data);
     function delete($id);
     function update(array $data, $id);
---
+++ app/GardenRevolution/Repositories/Contracts/UserRepositoryInterface.php
@@ -0,0 +1,11 @@
+<?php namespace App\GardenRevolution\Repositories\Contracts;
+
+/*
+ * Interface for user repository.
+ * @author Alan Ruvalcaba
+ * @since 2015-12-28
+ */
+interface UserRepositoryInterface extends Crud 
+{
+
+}
