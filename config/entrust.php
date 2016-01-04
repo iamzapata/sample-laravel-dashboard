58b9c7a9f8b9ad7cb40bf7ab9d973f85069d0135
Andres Zapata <me@azapata.io>
2016-01-04 13:47:27 -0500

    Update entrust config

--- config/entrust.php
+++ config/entrust.php
@@ -18,7 +18,7 @@ return [
     | the role if it is in a different namespace.
     |
     */
-    'role' => 'App\Models\Role',
+    'role' => 'App\Models\Roles\Role',
 
     /*
     |--------------------------------------------------------------------------
@@ -39,7 +39,7 @@ return [
     | Update the permission if it is in a different namespace.
     |
     */
-    'permission' => 'App\Models\Permission',
+    'permission' => 'App\Models\Permissions\Permission',
 
     /*
     |--------------------------------------------------------------------------
