c87ecf8adba6429433998088a87e1ba7c6937554
Andres Zapata <me@azapata.io>
2016-01-08 19:24:46 +0000

    Add backbonejs admin create user logic

--- resources/views/admin/dashboard/users/create.blade.php
+++ resources/views/admin/dashboard/users/create.blade.php
@@ -4,20 +4,20 @@
 <div class="row">
     <div class="form-group col-xs-4">
         {{ Form::label('username','Username') }}
-        {{ Form::text('username',$user->username,array('class'=>'form-control')) }}
+        {{ Form::text('username',null,array('class'=>'form-control')) }}
         <span class="validation-error"></span>
     </div>
 </div>
 <div class="row">
     <div class="form-group col-xs-4">
         {{ Form::label('email','Email') }}
-        {{ Form::text('email',$user->email,array('class'=>'form-control')) }}
+        {{ Form::text('email',null,array('class'=>'form-control')) }}
         <span class="validation-error"></span>
     </div>
 </div>
 <div class="row">
     <div class="form-group col-xs-4">
-        {{ Form::button('Update',array('class'=>'btn btn-success','id'=>'update')) }}
+        {{ Form::button('Create',array('class'=>'btn btn-success','id'=>'create')) }}
         <span class="validation-error"></span>
     </div>
 </div>
