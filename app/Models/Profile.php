cf80c0b0d1731068d198a02450ed1f1d01170597
Andres Zapata <me@azapata.io>
2016-01-12 15:58:19 -0500

    Add base model for profile

---
+++ app/Models/Profile.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Profile extends Model
+{
+    /*
+     * The attributes that are mass assignable
+     * @array
+     */
+    protected $fillable = ['first_name','last_name','street_address','city','state','zip','apt_suite'];
+}
