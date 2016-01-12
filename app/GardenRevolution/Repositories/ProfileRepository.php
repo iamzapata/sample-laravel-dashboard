a5beaa94ec06fe88ea4533e325b03ce12ff71103
Andres Zapata <me@azapata.io>
2016-01-12 18:05:55 -0500

    Add Profile Repository

---
+++ app/GardenRevolution/Repositories/ProfileRepository.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\GardenRevolutions\Repositories;
+
+use App\Models\Profile;
+
+use App\GardenRevolution\Repositories\Contracts\ProfileRepositoryInterface;
+
+class ProfileRepository extends ProfileRepositoryInterface
+{
+    /*
+     * @var Profile model
+     */
+    private $profile;
+
+    public function __construct(Profile $profile)
+    {
+        $this->profile = $profile;
+    }
+
+    /*
+     * @param array $data 
+     * @return bool
+     */
+    public function create(array $data)
+    {
+        $this->profile = $this->profile->newInstance()->fill($data);
+        return $this->profile->save();
+    }
+
+    /*
+     * @param array $data
+     * @param $id
+     * @return bool
+     */
+    public function update(array $data, $id)
+    {
+        $this->profile = $this->profile->newInstance()->find($id);
+
+        if( is_null($this->profile) ) 
+        {
+            return false;
+        }
+
+        $this->profile->fill($data);
+
+        return $this->profile->save();
+    }
+
+    /*
+     * @param $id
+     * @param array $columns
+     * @return $mixed
+     */
+    public function find($id, $columns = array('*'))
+    {
+        $this->profile = $this->profile->newInstance()->find($id,$columns);
+
+        return $this->profile;
+    }
+
+    /*
+     * @param $id
+     * @return bool|null
+     */
+    public function delete($id)
+    {
+        $this->profile = $this->profile->newInstance()->find($id);
+
+        if( is_null($this->profile) )
+        {
+            return false;
+        }
+
+        return $this->profile->delete();
+    }
+}
