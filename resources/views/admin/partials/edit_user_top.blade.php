1af5628b55c9377a2700dca2c22cd19907156e83
Andres Zapata <me@azapata.io>
2016-01-14 12:00:20 -0500

    Update edit and create user views

---
+++ resources/views/admin/partials/edit_user_top.blade.php
@@ -0,0 +1,38 @@
+<form id="userForm">
+    <div class="row">
+        <!-- left section -->
+        <div class="col-md-4">
+            <div class="form-group col-md-8">
+                {{ Form::label('username','Username') }}
+                {{ Form::text('username',$user->username,array('class'=>'form-control')) }}
+                <span class="validation-error"></span>
+            </div>
+            <div class="form-group col-md-8">
+                {{ Form::label('email','Email') }}
+                {{ Form::text('email',$user->email,array('class'=>'form-control')) }}
+                <span class="validation-error"></span>
+            </div>
+            <div class="form-group col-md-8">
+                {{ Form::label('password','Password') }}
+                {{ Form::password('password',array('class'=>'form-control')) }}
+                <span class="validation-error"></span>
+            </div>
+            <div class="form-group col-md-8">
+                {{ Form::label('password','Retype Password') }}
+                {{ Form::password('password_confirmation',array('class'=>'form-control')) }}
+                <span class="validation-error"></span>
+            </div>
+        </div>
+</form>
+        <!-- end of left section -->
+
+        <!-- middle section -->
+        <div class="col-md-4">
+        </div>
+        <!-- end of middle section -->
+
+        <!-- right section -->
+        <div class="col-md-4">
+        </div>
+        <!-- end of right section -->
+    </div>
