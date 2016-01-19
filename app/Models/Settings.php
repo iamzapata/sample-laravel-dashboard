f528070039ed83bbbf61bd68b9d8cd1119279855
Andres Zapata <me@azapata.io>
2016-01-19 12:27:55 -0500

    Add settings model

---
+++ app/Models/Settings.php
@@ -0,0 +1,14 @@
+<?php namespace
+
+use Illuminate\Database\Eloquent\Model;
+
+class Settings extends Model {
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        '','','',''
+    ];
+}
