98d32de7f7bb08cc708a36959b101cd9cc4d3353
Andres Zapata <me@azapata.io>
2016-01-23 03:41:33 +0000

    Add CustomerRepository

---
+++ app/GardenRevolution/Repositories/CustomerRepository.php
@@ -0,0 +1,35 @@
+<?php namespace App\GardenRevolution\Repositories;
+
+use Stripe\Customer;
+use App\GardenRevolution\Repositories\Contracts\CustomerRepositoryInterface;
+
+/*
+ * Repository for stripe customers
+ */
+class CustomerRepository implements CustomerRepositoryInterface {
+    private $customer;
+
+    public function __construct(Customer $customer) {
+        $this->customer = $customer;
+    }
+    
+    public function find($id) {
+        if( is_null($id) )
+        {
+            return null;
+        }
+
+        else 
+        {
+            try
+            {
+                return $this->customer->retrieve($id);  
+            }
+
+            catch(Exception $ex)
+            {
+                return null;
+            }
+        }
+    }
+}
