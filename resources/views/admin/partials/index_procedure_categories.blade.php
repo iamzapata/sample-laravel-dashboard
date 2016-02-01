96e0a50dd9df8e8f93ada62f90db776c185aa5ec
Andres Zapata <me@azapata.io>
2016-02-01 17:22:25 +0000

    Add partial views for category index

---
+++ resources/views/admin/partials/index_procedure_categories.blade.php
@@ -0,0 +1,30 @@
+<div class="row well procedure-categories">
+    <div class="panel panel-primary">
+        <div class="panel-heading">
+            <h3 class="panel-title">Procedure Categories</h3>
+        </div>
+        <div class="panel-body">
+            <table class="table">
+                <colgroup>
+                    <col class="col-xs-3">
+                    <col class="col-xs-3">
+                    <col class="col-xs-6">
+                </colgroup>
+                <tr>
+                    <th><u>Category Name</u></th>
+                    <th><u>Type</u></th>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td><a href="/admin/dashboard/#categories/create" class="btn btn-sm btn-success create-category">Create New</a>
+                <!--
+                <a href="/admin/dashboard/#categories/edit" class="btn btn-sm btn-primary edit-category">Edit</a>
+                <a href="/admin/dashboard/#categories/delete" class="btn btn-sm btn-danger delete-category">Delete</a>
+                -->
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </div>
+</div>
