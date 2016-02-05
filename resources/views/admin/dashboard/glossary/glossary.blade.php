a5511bcaae28be6f50e58a120c275ca4c322504d
Andres Zapata <me@azapata.io>
2016-02-05 23:33:23 +0000

    Add glossary index view

--- resources/views/admin/dashboard/glossary/glossary.blade.php
+++ resources/views/admin/dashboard/glossary/glossary.blade.php
@@ -1,2 +1,13 @@
 <h1> Glossary </h1>
-<p> List of glossary words. </p>
+
+<div class="row well">
+    <table class="table table-condensed">
+        <thead>
+            <tr>
+                <th class="col-xs-2"><u>Name</u></th>
+                <th class="col-xs-2"><u>Category</u></th>
+                <th class="col-xs-8"></th>
+            </tr>
+        </thead>
+    </table>
+</div>
