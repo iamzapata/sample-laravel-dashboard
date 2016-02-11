97ddf6b1f96b3220b802759744d1a528d97c840d
Andres Zapata <me@azapata.io>
2016-02-10 23:24:49 -0500

    Comment NotifyAction class methods

--- app/GardenRevolution/Notifications/NotifyAction.php
+++ app/GardenRevolution/Notifications/NotifyAction.php
@@ -6,17 +6,34 @@ namespace App\GardenRevolution\Notifications;
 /*
  * Class to output formatted action messages
  */
-public class NotifyAction {
-    public function added(string $entity) 
+
+
+class NotifyAction {
+    
+    /*
+    * Outputs a formatted string for entity added message notification
+    * @param $entity The entity type name
+    * @return The formatted entity added message
+    */
+    public function added($entity) 
     {
         return 'new {$entity} is added';
     }
 
+    /*
+     * Outputs a formatted string for attribute reminder message notification
+     * @param $attribute The attribute name
+     * @return The formatted attribute reminder message
+     */
     public function reminder(string $attribute)
     {
         return '{$attribute} reminder';
     }
-
+    
+    /*
+     * Outputs a general message notification
+     * @return The general message notification
+     */
     public function general()
     {
         return 'news and updates';
