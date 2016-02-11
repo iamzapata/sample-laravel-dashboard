b28ac50e11da0c51acb36af1c39f54a80276d580
Andres Zapata <me@azapata.io>
2016-02-10 21:54:08 -0500

    Add condition class for notifications

---
+++ app/GardenRevolution/Notifications/NotifyCondition.php
@@ -0,0 +1,9 @@
+<?php namespace App\GardenRevolution\Notifications;
+
+/*
+ * Describes the various notification conditions
+ */
+class NotifyCondition {
+    const PUBLISHED = 'on publish';
+    const TRIGGERED = 'when triggered';
+}
