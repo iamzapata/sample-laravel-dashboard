a10c976aa84a43369e03ba8530e8e09f5202eebf
Andres Zapata <me@azapata.io>
2016-02-21 02:22:41 +0000

    Add page index

--- resources/views/admin/dashboard/website-pages/website-pages.blade.php
+++ resources/views/admin/dashboard/website-pages/website-pages.blade.php
@@ -1,2 +1,21 @@
 <h1> Website Pages </h1>
-<p> List of website pages. </p>
+
+<div class="row well">
+    <table class="table table-condensed">
+        <thead>
+            <tr>
+                <th class="col-xs-2"><u>Title</u></th>
+                <th class="col-xs-8"></th>
+                <th class="col-xs-8"><u>Methods</u></th>
+            </tr>
+        </thead>
+            <tr>
+                <td></td><td></td><td><a href="#page/create" class="btn btn-success create-glossary">Add New</a></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+    </table>
+</div>
