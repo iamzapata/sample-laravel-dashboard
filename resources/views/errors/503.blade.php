73e8d9c3bc0a4dfc284238503cb70ce7706ffc20
Andres Zapata <me@azapata.io>
2016-02-23 21:03:32 -0500

    Edit maintenance mode view

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,13 +1,18 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Be right back.</title>
+        <title>Garden Revolution</title>
 
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
         <style>
+            @import url(https://fonts.googleapis.com/css?family=Raleway:400,600,300,700);
+
             html, body {
+                font-family: "Raleway";
                 height: 100%;
+                background: url('/images/green-house-bg.jpeg');
+                background-size: cover;
             }
 
             body {
@@ -32,15 +37,19 @@
             }
 
             .title {
-                font-size: 72px;
+                font-size: 75px;
                 margin-bottom: 40px;
+                font-weight: 900;
+                color: #fff;
+                text-shadow: 2px 2px rgba(0,0,0,0.5);
             }
         </style>
     </head>
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Be right back.</div>
+                <div class="title">Future Home Of</div>
+                <img src="/images/logo.png">
             </div>
         </div>
     </body>
